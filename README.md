82720880362457487f581b0c84612a212ee9a335
Fabio <fabio.ramos.semedo@gmail.com>
2024-11-24 17:04:10 +0000

    Notes_v1, Population continues

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Billionaires database uml:
   https://drive.google.com/file/d/1frab7zNKOdRgEPfRXAHRmG2ibd8yyO8S/view?usp=sharing
+
+Billionaires relational table diagram:
+  https://drive.google.com/file/d/1eKEHYa3Mql_C_MTgEtFfinolIDi_ds5E/view?usp=sharing
--- TablePopulation/parseSource.py
+++ TablePopulation/parseSource.py
@@ -4,8 +4,12 @@ with open('source.txt', 'r') as file:
         line = line.strip().split("\t")
         personId = int(line[0])
         sources = line[1].split(",")
+        
         for i in range(0,len(sources)):
-            print(f"{personId},{sources[i].strip()}")
+            sources[i] = sources[i].strip()       #Remove whitespaces around string
+            if sources[i][0].islower():           #Capitalised first letter
+                sources[i] = sources[i][0].upper() + sources[i][1:]
+            print(f"{personId},{sources[i]}")
         
         
 
