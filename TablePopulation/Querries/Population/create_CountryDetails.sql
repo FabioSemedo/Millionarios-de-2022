e022cf91def6814e8219c1c3b8c7c663aa87d129
Fabio <127618255+FabioSemedo@users.noreply.github.com>
2024-11-26 23:39:46 +0000

    9 OF 9!!! TBLs

--- TablePopulation/Querries/Population/create_CountryDetails.sql
+++ TablePopulation/Querries/Population/create_CountryDetails.sql
@@ -1,17 +1,25 @@
-CREATE TABLE CountryDetails (
-    countryNameID  INTEGER NOT NULL
-                           PRIMARY KEY,
-    cpi            REAL    NOT NULL,
-    cpi_change     REAL    NOT NULL,
-    gdp            INTEGER NOT NULL,
-    grs_trt_enroll REAL    NOT NULL,
-    grs_prm_enroll REAL    NOT NULL,
-    life_expect    INTEGER NOT NULL,
-    tax_rev        REAL    NOT NULL,
-    tax_rate       REAL    NOT NULL,
-    population     INTEGER NOT NULL,
-    latitude       REAL    NOT NULL,
-    longitude      REAL    NOT NULL,
-    continent      TEXT    NOT NULL
-    CHECK (continent IN ("Europe", "North America", "South America", "Asia", "Oceania", "Africa") ) 
-);
+INSERT INTO CountryDetails (cpi, cpi_change)
+select 1;
+
+SELECT c.countryNameID, t.continent,
+        t.country_lat, t.country_long, t.country_pop, 
+        t.cpi_country, t.cpi_change_country,
+        t.gdp_country, t.g_tertiary_ed_enroll, g_primary_ed_enroll,
+        t.life_expectancy, t.tax_revenue, t.tax_rate
+FROM CountryNames c INNER JOIN test t ON c.name = t.country_of_residence
+
+;
+
+--Check all countries only have one set of details associated to them
+SELECT country_of_residence, count(*)
+FROM (
+SELECT country_of_residence, continent,
+        country_lat, country_long, country_pop, 
+        cpi_country, cpi_change_country,
+        gdp_country, g_tertiary_ed_enroll, g_primary_ed_enroll,
+        life_expectancy, tax_revenue, tax_rate
+FROM test
+GROUP BY country_of_residence)
+GROUP BY Country_of_residence
+HAVING COUNT(*) > 1
+;
