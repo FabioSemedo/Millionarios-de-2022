14c65ad7033e3242702ee7d6dfc94e150ec62d3f
Fabio <fabio.ramos.semedo@gmail.com>
2024-11-29 01:12:14 +0000

    formated birth_dates

--- TablePopulation/Querries/Tests/format_dates.sql
+++ TablePopulation/Querries/Tests/format_dates.sql
@@ -1,11 +1,21 @@
---check dates
-select birth_day, birth_month, birth_date
-from test2
-where (birth_day > 31) OR (birth_month > 12) 
-    OR (birth_month < 1) OR (birth_day < 1 )
-    or ((birth_day >29) and (birth_month = 2))
-    ;
---format date
-update test2
-set birth_date = birth_year || "-" || birth_month || "-" || birth_day
-where true;
+-- SQLite
+--Rollback;
+BEGIN TRANSACTION;
+
+with tmp as (
+select birth_date as date, 
+    Case
+        when birth_month< 10 AND birth_day< 10 THEN birth_year || "-0" || birth_month || "-0" || birth_day
+        when birth_month< 10 THEN birth_year || "-0" || birth_month || "-" || birth_day
+        when birth_day< 10  THEN birth_year || "-" || birth_month || "-0" || birth_day
+        ELSE birth_year || "-" || birth_month || "-" || birth_day
+    END as format
+    FROM test2
+)
+update Test2
+set birth_date = format
+FROM tmp
+WHERE test2.birth_date = tmp.date
+RETURNING *;
+
+commit;
